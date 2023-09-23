f0383c733bbcd8274d81e8a3cb82f7fc9679ff9c
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-09-23 15:26:56 +0300

    feat: task servlets files add

---
+++ src/main/java/io/github/web22109231705/AreaCheckServlet.java
@@ -0,0 +1,9 @@
+package io.github.web22109231705;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "areaCheckServlet", value = "/area-check")
+public class AreaCheckServlet extends HttpServlet {
+
+}
---
+++ src/main/java/io/github/web22109231705/ControllerServlet.java
@@ -0,0 +1,9 @@
+package io.github.web22109231705;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "controllerServlet", value = "/controller")
+public class ControllerServlet extends HttpServlet {
+
+}
