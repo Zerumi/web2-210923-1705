0a36094fdabba28d998d9aef17a749fbe5e1a0f0
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-09-25 05:13:29 +0300

    feat: add models

---
+++ src/main/java/io/github/web22109231705/model/AreaData.java
@@ -0,0 +1,75 @@
+package io.github.web22109231705.model;
+
+import java.beans.JavaBean;
+import java.io.Serializable;
+import java.util.Objects;
+
+@JavaBean
+public class AreaData implements Serializable {
+    private double x;
+    private double y;
+    private double r;
+    private double result;
+
+    public AreaData() {
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AreaData)) return false;
+        AreaData areaData = (AreaData) o;
+        return Double.compare(this.getX(), areaData.getX()) == 0
+                && Double.compare(this.getY(), areaData.getY()) == 0
+                && Double.compare(this.getR(), areaData.getR()) == 0
+                && Double.compare(this.getResult(), areaData.getResult()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getX(), this.getY(), this.getR(), this.getResult());
+    }
+
+    @Override
+    public String toString() {
+        return "AreaData{" +
+                "x=" + x +
+                ", y=" + y +
+                ", r=" + r +
+                ", result=" + result +
+                '}';
+    }
+}
---
+++ src/main/java/io/github/web22109231705/model/UserAreaDatas.java
@@ -0,0 +1,43 @@
+package io.github.web22109231705.model;
+
+import java.beans.JavaBean;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+
+@JavaBean
+public class UserAreaDatas implements Serializable {
+    private List<AreaData> areaDataList;
+
+    public UserAreaDatas() {
+
+    }
+
+    public List<AreaData> getAreaDataList() {
+        return areaDataList;
+    }
+
+    public void setAreaDataList(List<AreaData> areaDataList) {
+        this.areaDataList = areaDataList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAreaDatas)) return false;
+        UserAreaDatas that = (UserAreaDatas) o;
+        return Objects.equals(getAreaDataList(), that.getAreaDataList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAreaDataList());
+    }
+
+    @Override
+    public String toString() {
+        return "UserAreaDatas{" +
+                "areaDataList=" + areaDataList +
+                '}';
+    }
+}
